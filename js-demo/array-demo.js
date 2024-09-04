f9d61fc119a7eec92c70c662507405bddb819663
eunseo Lee <147049456+ieunseo@users.noreply.github.com>
2024-09-04 23:47:43 +0900

    Update array-demo.js

--- js-demo/array-demo.js
+++ js-demo/array-demo.js
@@ -1,6 +1,6 @@
 //자바스크립트 배열 비구조화
-const arr = [1,2,3,4,5]
+const arr = [8,7,6,5,4,3,2,1,0]
 
-const[ , num2, num3, ,num5] = arr
-console.log(num2)
-console.log(num5)
+const[ , , num3, num4, , ,num7] = arr
+console.log(num4)
+console.log(num7)
