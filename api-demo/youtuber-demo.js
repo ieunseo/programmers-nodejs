6a924b150262a68f763d3abd4566dda2483296f2
eunseo Lee <147049456+ieunseo@users.noreply.github.com>
2024-09-04 23:46:43 +0900

    Update youtuber-demo.js

--- api-demo/youtuber-demo.js
+++ api-demo/youtuber-demo.js
@@ -21,30 +21,30 @@ let youtuber2 = {
 
 }
 
-let db = new Map()
+let yt = new Map()
 var id = 1
 
-db.set(id++,youtuber1)
-db.set(id++,youtuber2)
+yt.set(id++,youtuber1)
+yt.set(id++,youtuber2)
 
 //개별조회
 app.get('/youtuber/:id',function(req,res){
     let {id} = req.params
 
     id = parseInt(id)
-    if(db.get(id) == undefined){
+    if(yt.get(id) == undefined){
         res.json({
             message:"없는 유튜버"
         })
     }else{
-        res.json(db.get(id))
+        res.json(yt.get(id))
     }
 })
 
-//전체조회
+//전체조회 아직 미구현
 app.get('/youtubers',(req,res)=>{
     res.json({
-        db
+        res.json(Array.from(yt.values()));
     })
 })
 
@@ -54,8 +54,8 @@ app.use(express.json())
 app.post('/youtuber',(req,res)=>{
     console.log(req.body)
     //등록
-    db.set(id++,req.body)
+    yt.set(id++,req.body)
     res.json({
-        message:`${db.get(id-1).channelTitle}님,  유튜브 채널이 개설되었습니다. 화이팅!`
+        message:`${yt.get(id-1).channelTitle}님,  유튜브 채널이 개설되었습니다. 화이팅!`
     })
 })
