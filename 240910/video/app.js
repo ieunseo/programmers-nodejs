ed1fdcdddeeb1f1e8b0a606a4eeecc68810188d1
ieunseo <y949512@naver.com>
2024-09-14 21:56:40 +0900

    Changes

---
+++ 240910/video/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3300;
+app.use(express.json());
+app.listen(port, () => {
+    console.log(`Running at ${port}`);
+})
+
+const userRouter = require('./routes/user-demo');
+const loggerRouter = require('./routes/alog-demo');
+app.use("/",userRouter);
+app.use("/alog",loggerRouter);
