07f93b79ccfcdef013ef77a019036e65f378b1ba
ieunseo <y949512@naver.com>
2024-09-15 22:16:34 +0900

    금요일내용

--- 240913/video/db.js
+++ 240913/video/db.js
@@ -7,6 +7,7 @@ const connection = await mysql.createConnection({
     user: 'root',
     password:'1234',
     database: 'alogger',
+    dateStrings : true
 });
 
 // A simple SELECT query
@@ -15,8 +16,20 @@ try {
         'SELECT * FROM `users`'
     );
 
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
+    // console.log(results); // results contains rows returned by server
+    // console.log(fields); // fields contains extra meta data about results, if available
+
+    // 안에있는 내용을 꺼내기.
+    console.log('-------------------')
+    console.log(results[0]) // id가 1인 사용자의 내용 전체출력
+    console.log('-------------------')
+    console.log(results[0].id);
+    console.log(results[0].name);
+    console.log(results[0].email);
+    console.log('-------------------')
+    const { id , email , name, created_at} = results[2];
+    console.log(id,'+',email,'+',name,'+',created_at);
+
 } catch (err) {
     console.log(err);
 }
