84e2c8440e9db45537d11866ff5f7c16e2da3d6f
ieunseo <y949512@naver.com>
2024-09-23 22:34:57 +0900

    월요일 내용

--- 240923/video/routes/alog-demo.js
+++ 240923/video/routes/alog-demo.js
@@ -1,112 +1,127 @@
 const express = require('express');
-
 const router = express.Router();
+const conn = require('../db');
 
 router.use(express.json());
 
-let database = new Map();
-var id = 1;
-router
-    .route('/')
-    // 채널 생성
-    .post((req, res) => {
-        if(req.body.channelTitle) {
-            database.set(id++, req.body)
-            res.status(201).json({
-                message: `${database.get(id - 1).channelTitle}님의 기록을 응원합니다.`
-            })
-        }else{
-            res.status(400).json({
-                message : `요청값(양식) 이 잘못되었습니다.`
-                })
-        }
-    })
-    // 전체 조회
-    .get((req, res) => {
-        var alogger = []
-        var{userId} = req.body;
+// 채널 생성
+router.post('/', (req, res) => {
+    const { email, channelTitle } = req.body;
 
-        if(database.size) {
+    if (!email || !channelTitle) {
+        return res.status(400).json({
+            message: "요청값(양식)이 잘못되었습니다."
+        });
+    }
 
-            if(userId == undefined){
-                res.status(404).json({
-                    message:"로그인이 필요한 페이지입니다."
-                })
-            }else {
-                database.forEach(function (value, key) {
-                    if (value.userId === userId) {
-                        alogger.push(value)
-                    }
-                })
-                if(alogger.length === 0){
-                    res.status(404).json({
-                        message:"조회할 채널이 없습니다"
-                    })
-                }else{
-                console.log(alogger)
-                res.status(200).json(alogger)
-                }
-            }
+    const sql_query = `INSERT INTO channels (email, channelTitle) VALUES (?, ?)`;
+    conn.query(sql_query, [email, channelTitle], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
         }
-        else{
+
+        res.status(201).json({
+            message: `${channelTitle}님의 기록을 응원합니다.`,
+            results
+        });
+    });
+});
+
+// 전체 조회
+router.get('/', (req, res) => {
+    const { email } = req.query;  // 쿼리 파라미터로 email 받기
+
+    if (!email) {
+        return res.status(400).json({
+            message: "이메일이 필요합니다."
+        });
+    }
+
+    const sql_query = `SELECT * FROM channels WHERE email = ?`;
+    conn.query(sql_query, [email], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        if (results.length === 0) {
             res.status(404).json({
-                message:"조회할 채널이 없습니다"
-            })
-        }
-    })
-
-router
-    .route('/:id')
-    // 개별 조회
-    .get((req, res) => {
-        const id = parseInt(req.params.id);
-        const user = database.get(id);
-
-        if (user) {
-            res.status(200).json(user);
-        } else {
-            res.status(404).json({
-                message: "잘못된 채널정보"
+                message: "조회할 채널이 없습니다."
             });
+        } else {
+            res.status(200).json(results);
         }
-    })
-    // 개별 삭제
-    .delete((req, res) => {
-        const id = parseInt(req.params.id);
-        const user = database.get(id);
+    });
+});
 
-        if (user) {
-            database.delete(id)
+// 회원 개별 조회
+router.get('/users/:email', (req, res) => {
+    const email = req.params.email;
+
+    const sql_query = `SELECT * FROM users WHERE email = ?`;
+    conn.query(sql_query, [email], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        if (results.length === 0) {
+            res.status(404).json({
+                message: "회원을 찾을 수 없습니다."
+            });
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+
+// 회원 개별 삭제
+router.delete('/users/:email', (req, res) => {
+    const email = req.params.email;
+
+    const sql_query = `DELETE FROM users WHERE email = ?`;
+    conn.query(sql_query, [email], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        if (results.affectedRows > 0) {
             res.status(200).json({
-                message : `${user.channelTitle} 이 정상적으로 삭제되었습니다.`
+                message: `${email}님의 계정이 정상적으로 삭제되었습니다.`
             });
         } else {
             res.status(404).json({
-                message: "정보를 찾을수없습니다."
+                message: "회원을 찾을 수 없습니다."
             });
         }
-    })
+    });
+});
 
+// 채널 제목 수정
+router.put('/users/:email', (req, res) => {
+    const email = req.params.email;
+    const { channelTitle } = req.body;
 
-router.put('/:id',(req, res) => {
-    let {id} = req.params
-    id = parseInt(id)
-
-    var ytb = database.get(id)
-
-    if(ytb == undefined){
-        res.json({
-            message : `요청하신 ${id} 유튜버는 없는 블로거입니다.`
-        })
+    if (!channelTitle) {
+        return res.status(400).json({
+            message: "수정할 채널 제목을 입력해주세요."
+        });
     }
 
-    else{
-        let newTitle = req.body.channelTitle
-        ytb.channelTitle = newTitle;
-        database.set(id,ytb)
-        res.json({
-            message: `${req.body.channelTitle} 로 수정되었습니다.`
-        })
-    }
-})
+    const sql_query = `UPDATE channels SET channelTitle = ? WHERE email = ?`;
+    conn.query(sql_query, [channelTitle, email], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        if (results.affectedRows > 0) {
+            res.status(200).json({
+                message: `${channelTitle}로 수정되었습니다.`
+            });
+        } else {
+            res.status(404).json({
+                message: "회원을 찾을 수 없습니다."
+            });
+        }
+    });
+});
+
 module.exports = router;
